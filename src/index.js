218112567ef48d04d5794da903dc23a1da2fa035
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-06-06 20:43:36 +0000

    fix: foo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import { addServerPlugin, createResolver } from '@nuxt/kit'
+import { addServerPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
 
 const resolver = createResolver(import.meta.url)
 
-export default () => (options, nuxt) => {
-  nuxt.options.runtimeConfig.public.nuxtContentGit = {
-    createdAtName: 'createdAt',
-    updatedAtName: 'updatedAt',
-    ...nuxt.options.runtimeConfig.public.nuxtContentGit,
-    ...nuxt.options.nuxtContentGit,
-    ...options,
-  }
-  addServerPlugin(resolver.resolve('./server-plugin.js'))
-}
+export default defineNuxtModule({
+  setup: (options, nuxt) => {
+    nuxt.options.runtimeConfig.public.nuxtContentGit = {
+      createdAtName: 'createdAt',
+      updatedAtName: 'updatedAt',
+      ...nuxt.options.runtimeConfig.public.nuxtContentGit,
+      ...nuxt.options.nuxtContentGit,
+      ...options,
+    }
+    addServerPlugin(resolver.resolve('./server-plugin.js'))
+  },
+})
