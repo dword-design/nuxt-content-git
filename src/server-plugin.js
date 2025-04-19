e118b4b1dd98ba62e308a9057cf25a0905bc4631
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-19 09:56:19 +0000

    fix: simplification

--- src/server-plugin.js
+++ src/server-plugin.js
@@ -1,15 +1,20 @@
+import { last } from '@dword-design/functions';
 import P from 'path';
+import simpleGit from 'simple-git';
 
 import { defineNitroPlugin, useRuntimeConfig } from '#imports';
 
-import setVariables from './set-variables.js';
+const options = useRuntimeConfig().nuxtContentGit;
 
 export default defineNitroPlugin(nitroApp =>
-  nitroApp.hooks.hook('content:file:afterParse', file =>
-    setVariables(
-      file,
-      P.join('content', file._file),
-      useRuntimeConfig().nuxtContentGit,
-    ),
-  ),
+  nitroApp.hooks.hook('content:file:afterParse', async file => {
+    const git = simpleGit();
+    const log = await git.log({ file: P.join('content', file._file) });
+
+    file[options.createdAtName] =
+      log.all.length > 0 ? new Date(last(log.all).date) : file.createdAt;
+
+    file[options.updatedAtName] =
+      log.latest === null ? file.updatedAt : new Date(log.latest.date);
+  }),
 );
