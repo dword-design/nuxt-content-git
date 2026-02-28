33eb134050bb8c2c67eefc479cc1e2bf2ad59a4c
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-02-28 09:30:40 +0100

    fix: foo

--- src/module.ts
+++ src/module.ts
@@ -2,13 +2,7 @@ import type { FileAfterParseHook } from '@nuxt/content';
 import { defineNuxtModule } from '@nuxt/kit';
 import simpleGit from 'simple-git';
 
-declare module '@nuxt/schema' {
-  interface NuxtHooks {
-    'content:file:afterParse': (
-      ctx: FileAfterParseHook,
-    ) => Promise<void> | void;
-  }
-}
+// TODO: Somehow get the hook declared from @nuxt/content instead of redeclaring it here
 declare module 'nuxt/schema' {
   interface NuxtHooks {
     'content:file:afterParse': (
@@ -28,29 +22,23 @@ export default defineNuxtModule<{
     name: 'nuxt-content-git',
   },
   setup: (options, nuxt) => {
-    nuxt.hook(
-      'content:file:afterParse',
-      async ({ file, content }: FileAfterParseHook) => {
-        if (content[options.createdAtName] && content[options.updatedAtName]) {
-          return;
-        }
+    nuxt.hook('content:file:afterParse', async ({ file, content }) => {
+      if (content[options.createdAtName] && content[options.updatedAtName]) {
+        return;
+      }
 
-        const git = simpleGit();
-        const log = await git.log({ file: file.path });
+      const git = simpleGit();
+      const log = await git.log({ file: file.path });
 
-        if (!content[options.createdAtName]) {
-          const oldest = log.all.at(-1);
+      if (!content[options.createdAtName]) {
+        const oldest = log.all.at(-1);
+        content[options.createdAtName] = oldest ? new Date(oldest.date) : null;
+      }
 
-          content[options.createdAtName] = oldest
-            ? new Date(oldest.date)
-            : null;
-        }
-
-        if (!content[options.updatedAtName]) {
-          content[options.updatedAtName] =
-            log.latest === null ? null : new Date(log.latest.date);
-        }
-      },
-    );
+      if (!content[options.updatedAtName]) {
+        content[options.updatedAtName] =
+          log.latest === null ? null : new Date(log.latest.date);
+      }
+    });
   },
 });
